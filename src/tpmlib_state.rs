2425a8f7b2b19efe789c2ba6848223f93ac4d8a4
Steven Malis <stevenmalis@microsoft.com>
2026-07-16 12:19:09 -0400

    Simplify build.rs

--- src/tpmlib_state.rs
+++ src/tpmlib_state.rs
@@ -7,7 +7,6 @@ use crate::error::Error;
 use serde::Deserialize;
 use serde::Serialize;
 
-#[link(name = "runtime_state", kind = "static")]
 unsafe extern "C" {
     // Returns:
     // - 0 on success
